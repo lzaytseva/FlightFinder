1f2540f54f79b3652d3475fa8d3fe97ff3910da9
ledrabbit <lvzaytseva1@gmail.com>
2024-04-19 20:00:12 +0300

    removed unused import

--- app/src/main/java/com/github/lzaytseva/flightfinder/MainActivity.kt
+++ app/src/main/java/com/github/lzaytseva/flightfinder/MainActivity.kt
@@ -2,7 +2,6 @@ package com.github.lzaytseva.flightfinder
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {

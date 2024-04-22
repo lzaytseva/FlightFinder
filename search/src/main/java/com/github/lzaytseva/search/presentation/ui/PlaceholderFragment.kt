dad614812b0275ae4b883951f4115469f00f7af6
ledrabbit <lvzaytseva1@gmail.com>
2024-04-22 23:22:45 +0300

    fixed navigation from search fragment to placeholder

--- search/src/main/java/com/github/lzaytseva/search/presentation/ui/PlaceholderFragment.kt
+++ search/src/main/java/com/github/lzaytseva/search/presentation/ui/PlaceholderFragment.kt
@@ -1,13 +1,23 @@
 package com.github.lzaytseva.search.presentation.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.github.lzaytseva.search.R
 
 internal class PlaceholderFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_placeholder, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
